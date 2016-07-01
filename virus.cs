e5f1ccd17be1fa9cd0339a5f3afae2eeaab79f2f
ChickenStorm <tetras.aigle@yahoo.fr>
2016-07-01 22:53:09 +0200

    remove sa and virus

---
+++ virus.cs
@@ -0,0 +1,15 @@
+
+
+
+
+pubic class Virus: Entity{
+	
+	public Virus(vecteur3 position, vecteur3 vitesse,int width, int heigth): base(position,vitesse,false,width,heigth){
+		
+	}
+	
+	
+	
+	
+1	
+}
